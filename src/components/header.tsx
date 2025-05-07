a43e8153c62549550a900fadd5b3869bfbb76dc7
LeeSeokmin <dltjrals13@naver.com>
2025-05-07 10:45:52 +0900

    Refactor header component for improved readability
    - Reformatted the JSX structure in the Header component for better readability and maintainability, ensuring a cleaner codebase while preserving existing functionality.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,22 +6,22 @@ export default function Header() {
   const alertServicePreparing = () => {
     useAuthStore.getState().setAlert("서비스 준비중입니다.", "info");
   }
-    return (
-        <header className="fixed w-full backdrop-blur-md bg-background/70 z-40 border-b border-component-border">
-        <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
-          <Link href="/">
-            <Logo />
-          </Link>
-          <nav className="md:flex space-x-10 text-sm font-medium">
-            <Link href="/platform" className="hover:text-point-color-purple transition-colors duration-200">플랫폼</Link>
-            <Link href="#" className="hover:text-point-color-purple transition-colors duration-200" onClick={alertServicePreparing}>커뮤니티</Link>
-            <Link href="#" className="hover:text-point-color-purple transition-colors duration-200" onClick={alertServicePreparing}>멘토링</Link>
-            <Link href="#" className="hover:text-point-color-purple transition-colors duration-200" onClick={alertServicePreparing}>이벤트</Link>
-          </nav>
-          <Link href="/signin" className="text-white bg-point-color-purple hover:bg-point-color-purple-hover px-5 py-2 rounded-lg text-sm font-medium transition-colors duration-200">
-            시작하기
-          </Link>
-        </div>
-      </header>
-    )
+  return (
+    <header className="fixed w-full backdrop-blur-md bg-background/70 z-40 border-b border-component-border">
+      <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
+        <Link href="/">
+          <Logo />
+        </Link>
+        <nav className="md:flex space-x-10 text-sm font-medium">
+          <Link href="/platform" className="hover:text-point-color-purple transition-colors duration-200">플랫폼</Link>
+          <Link href="#" className="hover:text-point-color-purple transition-colors duration-200" onClick={alertServicePreparing}>커뮤니티</Link>
+          <Link href="#" className="hover:text-point-color-purple transition-colors duration-200" onClick={alertServicePreparing}>멘토링</Link>
+          <Link href="#" className="hover:text-point-color-purple transition-colors duration-200" onClick={alertServicePreparing}>이벤트</Link>
+        </nav>
+        <Link href="/signin" className="text-white bg-point-color-purple hover:bg-point-color-purple-hover px-5 py-2 rounded-lg text-sm font-medium transition-colors duration-200">
+          시작하기
+        </Link>
+      </div>
+    </header>
+  )
 }
