2a1d3b458a765564df6a485823d7439326144ae8
LeeSeokmin <dltjrals13@naver.com>
2025-05-07 13:23:50 +0900

    Update server configuration to use environment variable for base URL

--- src/auth/server.ts
+++ src/auth/server.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const server = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: process.env.PUBLIC_API_URL,
   headers: {
     'Content-Type': 'application/json',
   }
