67997d422205ee83a9c57c01be5d753d52a8b84f
LeeSeokmin <dltjrals13@naver.com>
2025-05-23 15:23:27 +0900

    Update color mapping in getPriorityColor and getStatusColor functions
    - Adjusted color values in getPriorityColor to ensure consistency in background and text colors for priority levels.
    - Modified getStatusColor to change colors for 'not-started' and 'done' statuses, enhancing visual distinction in status representation.

--- src/utils/getStatusColor.ts
+++ src/utils/getStatusColor.ts
@@ -1,10 +1,10 @@
 export const getStatusColor = (status: string) => {
   switch (status) {
     case 'not-started':
-      return 'bg-yellow-500/20 text-yellow-500';
+      return 'bg-fuchsia-500/20 text-fuchsia-500';
     case 'in-progress':
       return 'bg-blue-500/20 text-blue-500';
     case 'done':
-      return 'bg-green-500/20 text-green-500';
+      return 'bg-emerald-500/20 text-emerald-500';
   }
 };
