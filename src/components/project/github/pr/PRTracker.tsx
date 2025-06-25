f5a131ccf122f5f80b6e17a17b705d4c60d97ee0
LeeSeokmin <dltjrals13@naver.com>
2025-06-25 10:58:03 +0900

    Refactor layout of IssueTracker and PRTracker components for improved responsiveness
    - Updated the layout of the search input and state selection components to enhance responsiveness across different screen sizes.
    - Adjusted class names to ensure consistent styling and alignment within the flex container.

--- src/components/project/github/pr/PRTracker.tsx
+++ src/components/project/github/pr/PRTracker.tsx
@@ -38,26 +38,30 @@ export default function PRTracker({ prData }: PRTrackerProps) {
         <PRCountCard prCount={prData.filter((pr) => pr.state === "closed").length || 0} state="closed" />
         <PRAverageResolutionTimeCard prs={prData} />
       </div>
-      <div className="flex items-center flex-col md:flex-row gap-2 justify-between">
-        <Input
-          placeholder="PR 검색..."
-          value={searchQuery}
-          onChange={handleSearch}
-          className="w-full py-2 !rounded-md !bg-component-background"
-          fullWidth
-          startAdornment={<Search className="h-5 w-5 text-gray-400" />}
-        />
-        <Select
-          options={[
-            { name: "state", value: "all", label: "모든 상태" },
-            { name: "state", value: "open", label: "열림" },
-            { name: "state", value: "closed", label: "닫힘" },
-          ]}
-          value={selectedState}
-          onChange={(e) => onSelectState(e as string)}
-          className="w-full md:w-1/5 p-2 !rounded-md !bg-component-background !border !border-component-border"
-          dropDownClassName="!w-full !rounded-md"
-        />
+      <div className="flex w-full flex-col gap-2 md:flex-row md:items-baseline">
+        <div className="w-full md:w-4/5">
+          <Input
+            placeholder="PR 검색..."
+            value={searchQuery}
+            onChange={handleSearch}
+            className="!w-full !rounded-md !bg-component-background"
+            fullWidth
+            startAdornment={<Search className="h-5 w-5 text-gray-400" />}
+          />
+        </div>
+        <div className="w-full md:w-1/5">
+          <Select
+            options={[
+              { name: "state", value: "all", label: "모든 상태" },
+              { name: "state", value: "open", label: "열림" },
+              { name: "state", value: "closed", label: "닫힘" },
+            ]}
+            value={selectedState}
+            onChange={(e) => onSelectState(e as string)}
+            className="!w-full !rounded-md !bg-component-background !border !border-component-border"
+            dropDownClassName="!w-full !rounded-md"
+          />
+        </div>
       </div>
       {[...(filteredPRs || [])]
         .sort((a, b) => {
