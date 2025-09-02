382faab9a28add240ad8152f6a85e46e287a6ede
LeeSeokmin <dltjrals13@naver.com>
2025-09-02 11:31:18 +0900

    Add "use client" directive to Badge component for client-side rendering support

--- src/components/ui/Badge.tsx
+++ src/components/ui/Badge.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import { useTheme } from "@/contexts/ThemeContext";
