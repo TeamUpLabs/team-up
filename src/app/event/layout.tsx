4b110853af0879fc9b1c9bfe867c0e197d30eb24
LeeSeokmin <dltjrals13@naver.com>
2025-10-07 15:10:45 +0900

    Add Header component and update EventLayout
    - Introduced a new Header component to enhance navigation and layout consistency across event-related pages.
    - Updated EventLayout to include the Header and wrap children in a main section, improving overall structure and styling.
    - Ensured the Header features a breadcrumb navigation and user dropdown for better user experience.

--- src/app/event/layout.tsx
+++ src/app/event/layout.tsx
@@ -1,7 +1,14 @@
+"use client";
+
+import Header from "@/components/event/Header";
+
 export default function EventLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex-col min-h-screen bg-background">
-      {children}
+      <Header />
+      <main className="max-w-5xl mx-auto py-10">
+        {children}
+      </main>
     </div>
   );
 }
---
+++ src/components/event/Header.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import UserDropdown from "@/components/platform/UserDropdown";
+import { Home } from "lucide-react";
+import SlideingMenu, { IconProps } from "@/components/ui/SlideingMenu";
+
+const icons: IconProps[] = [
+  {
+    icon: <Home className="w-5 h-5" />,
+    label: "Home",
+    href: "/",
+  }
+]
+
+export default function Header() {
+
+  return (
+    <div className="w-full">
+      <header className="sticky top-0 z-10 bg-component-background min-h-10 border-b border-component-border">
+        <div className="px-3 py-2 flex items-center gap-3 w-full justify-between">
+          <div className="flex items-center gap-2">
+            <SlideingMenu icons={icons} />
+
+            <div className="flex flex-col">
+              <nav aria-label="Breadcrumb">
+                <ol className="flex items-center gap-2 text-xs">
+                  <li>
+                    <span className="font-medium text-point-color-purple">Dashboard</span>
+                  </li>
+                  <li className="rtl:rotate-180">
+                    <span className="text-text-secondary">/</span>
+                  </li>
+                  <li>
+                    <span className="font-medium text-text-primary">Events</span>
+                  </li>
+                </ol>
+              </nav>
+              <span className="font-semibold text-text-primary">Events</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-6">
+            <UserDropdown />
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
