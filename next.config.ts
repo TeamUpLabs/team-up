3602c9679c547f2ccbc36d8a6544a5a870b34e02
LeeSeokmin <dltjrals13@naver.com>
2025-10-10 14:41:11 +0900

    Update configuration and utility imports for improved functionality
    - Added 'picsum.photos' to the list of allowed image sources in the Next.js configuration, expanding the range of image options available for the application.
    - Refactored import statements in utils.ts for consistency, aligning with standard formatting practices.

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,10 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'randomuser.me',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
       }
     ],
     formats: ['image/webp', 'image/avif'],
